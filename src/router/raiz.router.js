d82496a23c80f37bb350c215f1f9828d4f86b685
fatt-chong <fatt.guillermo.chong.antilef@gmail.com>
2022-02-21 15:01:30 -0300

    se agrego la ruta para listar usuarios

--- src/router/raiz.router.js
+++ src/router/raiz.router.js
@@ -8,12 +8,40 @@ router.get("/", (req, res) => {
     });
 });
 
+//ruta para registrar el usuario
 router.get("/usuarios", (req, res) =>{
+
     User.create({
-        firstName : "fatt",
+        firstName : "fatt guillermo",
         lastName : "chong"
     }).then(user=>{
         res.json(user);
+    });
+    
+});
+
+//ruta para listar el usuarios
+router.get("/usuarios/listar", (req, res) => {
+    User.findAll().then((users) => {
+
+        if(users.length <= 0){
+            res.status(400).json({
+                "status" : 400,
+                "type" : "error",
+                "message" : "No hay registros de usuarios"
+            });
+        }else{
+            res.status(200).json(users);
+        }
+
+    }).catch((err) => {
+
+        res.status(500).json({
+            "status" : 500,
+            "type" : "error",
+            "message" : "Error al listar usuario, error: " + err.message
+        });
+
     });
 });
 

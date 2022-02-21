e55b40f4d9696a3b22d3782d96d24eb522f7adb2
fatt-chong <fatt.guillermo.chong.antilef@gmail.com>
2022-02-21 16:31:41 -0300

    comenzando la validacion del model

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -4,15 +4,14 @@ const sequelize = require("../db");
 class User extends Model {}
 
 User.init({
-  // Model attributes are defined here
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  lastName: {
-    type: DataTypes.STRING
-    // allowNull defaults to true
-  }
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+
+    role: {
+        type : DataTypes.INTEGER,
+        defaultValue : 0
+    }
 }, {
   // Other model options go here
   sequelize, // We need to pass the connection instance
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.listen(port, ()=>{
     console.log("servidor en el puerto: " + port);
     //conexion a la DB 
     
-    sequelize.sync({force: false}).then(()=>{
+    sequelize.sync({force: true}).then(()=>{
         console.log("conectado a la DB");
     }).catch((err)=>{
         console.log("error en: " + err.message);

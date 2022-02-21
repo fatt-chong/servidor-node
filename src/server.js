41cbbfc8410daff06a2c3a2a88c44e75d1ead0c4
fatt-chong <fatt.guillermo.chong.antilef@gmail.com>
2022-02-21 15:29:23 -0300

    se crea la ruta para los posts

--- src/server.js
+++ src/server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const { use } = require('./router/raiz.router');
 const app = express();
 const cors = require("cors");
+const rutaRaiz = require("./router/raiz.router");
+const postRutas = require("./router/posts.router");
 
 //middlewares de servidor
 app.use(cors());
 app.use(express.json());
 
 //rutas
-const rutaRaiz = require("./router/raiz.router");
 app.use("/", rutaRaiz);
+app.use("/posts", postRutas);
 
 module.exports = app;
